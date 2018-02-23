324e463ad2bea5a721c73c1a07ff8f36e277fe1b
eyal <eyal.merhavi@gmail.com>
2018-02-23 11:58:58 +0200

    added saving and loading grids from storage

--- Grid.py
+++ Grid.py
@@ -1,9 +1,9 @@
 import random
 # the grid object, which I use to store griddlers. includes some useful functions for easy use.
 class grid(object):
-    def __init__(self,size,option):
+    def __init__(self,name,size,option):
         self.size = size
-
+        self.name = name
         # options for creating grid: 0=randomize,1=blank
         if option==0:
             self.body = [[random.choice(['X',' ']) for i in range(size)] for i in range(size)]
@@ -27,6 +27,23 @@ class grid(object):
     def erase(self,row,col):
         self.body[row][col] = ' '
 
+    def save(self):
+        with open("grid_storage",'r+') as f:
+            cont = f.readlines()
+            if self.name+'\n' in cont:
+                print self.name+" already exists!"
+                return
+            f.write(self.name+'\n'+str(self.body)+'\n')
+
+    def load(self,name):
+        self.name = name
+        with open("grid_storage") as f:
+            content = f.readlines()
+        for i in range(len(content)):
+            if content[i].strip()==name:
+                self.body=eval(content[i+1].strip())
+                self.size=len(self.body)
+                return
 # the algorithm which describes a line/column, griddler style (distinguishing the "blobs" in aline and counting them)
 def desc_line(line):
     c = 0
--- main.py
+++ main.py
@@ -2,81 +2,86 @@ import pygame
 from Grid import *
 """Move class to different file"""
 
-# create grid object to base game window on
-g = grid(10, 1)
 
-# Setting some constants for the game window.
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREY = (128,128,128)
-WIDTH = 20
-HEIGHT = 20
-MARGIN = 5
-FRAME = g.size * 8
+#lets the user create a grid (with gui using pygame) and return it. n is the size (n*n) of the grid, name is name.
+def create_grid(name,n):
+    # create grid object to base game window on
+    g = grid(name,n, 1)
 
-# initialize the pygame module.
-pygame.init()
-# set the size of the window according to the size of the grid.
-size = int(g.size * 25.5)
-WINDOW_SIZE = [size+FRAME, size+FRAME]
-screen = pygame.display.set_mode(WINDOW_SIZE)
+    # Setting some constants for the game window.
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREY = (128,128,128)
+    WIDTH = 20
+    HEIGHT = 20
+    MARGIN = 5
+    FRAME = g.size * 8
 
-pygame.display.set_caption("Griddlers maker")
+    # initialize the pygame module.
+    pygame.init()
+    # set the size of the window according to the size of the grid.
+    size = int(g.size * 25.5)
+    WINDOW_SIZE = [size+FRAME, size+FRAME]
+    screen = pygame.display.set_mode(WINDOW_SIZE)
 
-done = False
+    pygame.display.set_caption("Griddlers maker")
 
-clock = pygame.time.Clock()
-# choose font for hint numbers
-font = pygame.font.SysFont("arial", 15)
+    done = False
 
-
-
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            column = (pos[0]-FRAME) // (WIDTH + MARGIN)
-            row = (pos[1]-FRAME) // (HEIGHT + MARGIN)
-            print("Click ", pos, "Grid coordinates: ", row, column)
-            if event.button == 1:
-                g.fill(row, column)
-            elif event.button ==3:
-                g.erase(row, column)
+    clock = pygame.time.Clock()
+    # choose font for hint numbers
+    font = pygame.font.SysFont("arial", 15)
 
 
 
-    screen.fill(BLACK)
-    """add line description in right places"""
-
-    # render and paint the horizontal and vertical text hints
-    for i in range(g.size):
-        line = desc_line(g.row(i))
-        text = font.render("  ".join(line), True,WHITE)
-        screen.blit(text,(FRAME-len(line)*12,FRAME+i*(HEIGHT+MARGIN)+7))
-        line = desc_line(g.col(i))
-        # no easy way to blit text with multiple lines in pygame, so i have to do it one num at a time
-        for j in range(len(line)):
-            text = font.render(line[j], True, WHITE)
-            screen.blit(text, (FRAME + i * (HEIGHT + MARGIN)+7,FRAME-(len(line)-j)*15))
-
-    #paint grid
-    for row in range(g.size):
-        for column in range(g.size):
-            color = WHITE
-            if g.body[row][column] == 'X':
-                color = GREY
-            pygame.draw.rect(screen,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN + FRAME,
-                              (MARGIN + HEIGHT) * row + MARGIN + FRAME,
-                              WIDTH,
-                              HEIGHT])
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                column = (pos[0]-FRAME) // (WIDTH + MARGIN)
+                row = (pos[1]-FRAME) // (HEIGHT + MARGIN)
+                print("Click ", pos, "Grid coordinates: ", row, column)
+                if event.button == 1:
+                    g.fill(row, column)
+                elif event.button ==3:
+                    g.erase(row, column)
 
 
-    clock.tick(60)
 
-    pygame.display.flip()
-pygame.quit()
-g.draw()
+        screen.fill(BLACK)
+        """add line description in right places"""
+
+        # render and paint the horizontal and vertical text hints
+        for i in range(g.size):
+            line = desc_line(g.row(i))
+            text = font.render("  ".join(line), True,WHITE)
+            screen.blit(text,(FRAME-len(line)*12,FRAME+i*(HEIGHT+MARGIN)+7))
+            line = desc_line(g.col(i))
+            # no easy way to blit text with multiple lines in pygame, so i have to do it one num at a time
+            for j in range(len(line)):
+                text = font.render(line[j], True, WHITE)
+                screen.blit(text, (FRAME + i * (HEIGHT + MARGIN)+7,FRAME-(len(line)-j)*15))
+
+        #paint grid
+        for row in range(g.size):
+            for column in range(g.size):
+                color = WHITE
+                if g.body[row][column] == 'X':
+                    color = GREY
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN + FRAME,
+                                  (MARGIN + HEIGHT) * row + MARGIN + FRAME,
+                                  WIDTH,
+                                  HEIGHT])
+
+
+        clock.tick(60)
+
+        pygame.display.flip()
+    pygame.quit()
+    g.save()
+    return g
+
